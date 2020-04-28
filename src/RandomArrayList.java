6f3000b6810ef587798aeb71e2356f68e6594cb9
EyobHAmenu <heyob888@gmail.com>
2020-04-28 14:07:52 -0400

    Second

--- src/RandomArrayList.java
+++ src/RandomArrayList.java
@@ -5,43 +5,43 @@ import java.util.Scanner;
 public class RandomArrayList {
     public static void main(String[] args) {
         /*
-            How would you return a random item from an ArrayList?
-
             Create 3 array list: subject, verb, and object.
-            Ask a user to populate each array list with 3 to 10 words.
-            As an example, ask a user to enter 4 subject word, 5 verb words,
-            and 9 object words. Ask a user if they want to generate a sentence.
+            Prompt user to populate each array list with 5 words.
+            Prompt user if they want to generate a sentence.
             If user says "yes" then create a sentence by randomly selecting one
             word from subject, verb and object array list.
             Finally, print out the content of your array lists.
 
-            case 1:
-                Enter verb 1:eat
-                Enter verb 2:drink
-                Enter verb 3:play
-                Enter verb 4:hit
-                Enter verb 5:kick
+            case :
+                Populate the verb arraylist.
+                Enter []1:eat
+                Enter [eat]2:drink
+                Enter [eat, drink]3:play
+                Enter [eat, drink, play]4:hit
+                Enter [eat, drink, play, hit]5:kick
                 [eat, drink, play, hit, kick]
                 ----------------------------
-                Enter subject 1:woman
-                Enter subject 2:man
-                Enter subject 3:boy
-                Enter subject 4:girl
-                Enter subject 5:child
-                [woman, man, boy, girl, child]
+                Populate the subject arraylist.
+                Enter []1:woman
+                Enter [woman]2:man
+                Enter [woman, man]3:boy
+                Enter [woman, man, boy]4:girl
+                Enter [woman, man, boy, girl]5:boy
+                [woman, man, boy, girl, boy]
                 ----------------------------
-                Enter object 1:orange
-                Enter object 2:ball
-                Enter object 3:chocolate
-                Enter object 4:coffee
-                Enter object 5:wall
+                Populate the object arraylist.
+                Enter []1:orange
+                Enter [orange]2:ball
+                Enter [orange, ball]3:chocolate
+                Enter [orange, ball, chocolate]4:coffee
+                Enter [orange, ball, chocolate, coffee]5:wall
                 [orange, ball, chocolate, coffee, wall]
                 ----------------------------
                 Do you want want to generate a sentence?(yes/no)
                 yes
-                Sentence created by the Random Generator: man kick orange
+                Sentence created by the Random Generator: boy drink orange
                 Have good day!!
-            case 2:
+
 
          */
 
@@ -63,30 +63,23 @@ public class RandomArrayList {
 
         // adding verbs to the verb array list.
         //built,eat,throw,hit,drink
-        for (int i = 1; i <= 5; i++) {
-            System.out.print("Enter verb " + i +":");
-            verb.add(in.nextLine());
-        }
+        System.out.println("Populate the verb arraylist.");
+        populate(verb,in);
         System.out.println(verb);
         System.out.println("----------------------------");
 
         // adding subject to the subject array list.
         // Woman,man, boy,child,girl
-        for (int i = 1; i <= 5; i++) {
-            System.out.print("Enter subject " + i +":");
-            subject.add(in.nextLine());
-        }
+        System.out.println("Populate the subject arraylist.");
+        populate(subject,in);
         System.out.println(subject);
         System.out.println("----------------------------");
 
         // adding words to the object array list.
         // chocolate,orange,coffee,ball,wall
-        for (int i = 1; i <= 5; i++) {
-            System.out.print("Enter object " + i +":");
-            object.add(in.nextLine());
-        }
+        System.out.println("Populate the object arraylist.");
+        populate(object,in);
         System.out.println(object);
-
         System.out.println("----------------------------");
 
         System.out.println("Do you want want to generate a sentence?(yes/no)");
@@ -98,6 +91,14 @@ public class RandomArrayList {
                     + subject.get(randomSubject) + " " + verb.get(randomVerb) + " " + object.get(randomObject));
         }
         System.out.println("Have good day!!");
+    }
+
+    public static void populate (ArrayList<String> name, Scanner sc) {
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Enter " + name + i +":");
+            name.add(sc.nextLine());
+        }
 
     }
 }
